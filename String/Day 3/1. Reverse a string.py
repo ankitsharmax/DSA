3807fa2643fd69c000a54e3f129c55ec328e7cad
UchihaUzumaki1x <kumarankitx022@gmail.com>
2020-11-01 19:33:03 +0530

    Day 3, 1 - Reverse a string

---
+++ String/Day 3/1. Reverse a string.py
@@ -0,0 +1,14 @@
+'''
+Day 3:
+    - 1. Reverse a string
+    Input: Hello
+    Output: olleH
+
+'''
+
+testcase = int(input())
+for _ in range(testcase):
+    text = input()
+    print(text)
+    print(text[::-1])
+    
